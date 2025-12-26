55312d4cb1fc57a1b1228eae42d90c725f21b39a
Hhttp01 <osismadar16@gmail.com>
2025-12-26 22:53:17 +0200

    Add MarketScout class with find_hot_product method

---
+++ modules/market_scout.py
@@ -0,0 +1,4 @@
+class MarketScout:
+    def find_hot_product(self, niche):
+        # כאן תבוא לוגיקת Pytrends - כרגע מחזיר דאמי לניסיון
+        return {"url": "https://example.com/item", "name": niche}
